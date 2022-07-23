a3509c06e271acd102bf5a04171dce7f470a8417
Divy <divyparekh1810@gmail.com>
2022-07-23 12:30:57 +0530

    done witth Reducers

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Slider } from "@mui/material";
 import React from "react";
 import "./Sidebar.css";
 
@@ -20,6 +21,66 @@ const Sidebar = () => {
           <option value="insertion">Insertion Sort</option>
           <option value="selection">Selection Sort</option>
           <option value="quick">Quick Sort</option>
+        </select>
+      </div>
+
+      <div className="sidebar__option">
+        <label htmlFor="range">Range: </label>
+        <Slider
+          style={{ width: "180px" }}
+          size="small"
+          defaultValue={30}
+          id="slider"
+          min={1}
+          className="slider"
+          //   disabled={myState.play ? true : false}
+          //   max={max}
+          //   onChange={(e) => handleRange(e.target.value)}
+          valueLabelDisplay="auto"
+        />
+      </div>
+
+      <div className="sidebar__option">
+        <label htmlFor="color">Color: </label>
+        <select
+          name="color"
+          id="color"
+          //   onChange={(e) => handleColor(e.target.value)}
+          //   disabled={myState.play ? true : false}
+        >
+          <option
+            value="rgb(0, 149, 199)"
+            style={{ color: "rgb(0, 149, 199)" }}
+          >
+            Blue
+          </option>
+          <option value="rgb(85, 212, 0)" style={{ color: "rgb(10,200,20)" }}>
+            Green
+          </option>
+          <option value="rgb(255, 112, 112)" style={{ color: "red" }}>
+            Red
+          </option>
+          <option value="grey" style={{ color: "grey" }}>
+            Black
+          </option>
+          <option value="#ddd902" style={{ color: "#ddd902" }}>
+            Yellow
+          </option>
+        </select>
+      </div>
+
+      <div className="sidebar__option">
+        <label htmlFor="speed">Speed: </label>
+        <select
+          name="speed"
+          defaultValue={100}
+          id="speed"
+          //   onChange={(e) => handleSpeed(e.target.value)}
+          //   disabled={myState.play ? true : false}
+        >
+          <option value={500}>Slow</option>
+          <option value={200}>Medium</option>
+          <option value={100}>Fast</option>
         </select>
       </div>
     </div>
--- src/components/Visuals.jsx
+++ src/components/Visuals.jsx
@@ -1,7 +1,58 @@
 import React from "react";
-
+import "./Visuals.css";
 const Visuals = () => {
-  return <div>Visuals</div>;
+  return (
+    <>
+      <div className="visuals">
+        <div className="visualizer">
+          <div className="visual__items">
+            <div
+              className="visual__item"
+              // key={item[1]}
+              // id={item[1]}
+              // style={{
+              //   transition: `${speed / 1000}s linear all`,
+              //   transform: `translateX(${item[1] * 11}px)`,
+              // }}
+            >
+              <h4>12</h4>
+              <div
+                className="visual"
+                style={{
+                  height: "36px",
+                  backgroundColor: "#000",
+                  width: "8px",
+                }}
+              ></div>
+            </div>
+            <div
+              className="visual__item"
+              // key={item[1]}
+              // id={item[1]}
+              // style={{
+              //   transition: `${speed / 1000}s linear all`,
+              //   transform: `translateX(${item[1] * 11}px)`,
+              // }}
+            >
+              <h4>12</h4>
+              <div
+                className="visual"
+                style={{
+                  height: "66px",
+                  backgroundColor: "#000",
+                  width: "8px",
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
+        <div className="visual__btns">
+          <button id="change-btn">change values</button>
+          <button id="play-btn">play</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Visuals;
