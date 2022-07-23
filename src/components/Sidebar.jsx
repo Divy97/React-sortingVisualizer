ba6c677d6a4d3a5127b2fb8c3ad0e5c1eb8c81e0
Divy <divyparekh1810@gmail.com>
2022-07-23 14:34:02 +0530

    Done With Bubble Sort

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,84 @@
-import { Slider } from "@mui/material";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./Sidebar.css";
+import { Slider } from "@mui/material";
 
-const Sidebar = () => {
+function Sidebar() {
+  const myState = useSelector((state) => state.updateActions);
+  const dispatch = useDispatch();
+
+  const [max, setMax] = useState(30);
+
   const handleAlgo = (algo) => {
-    console.log("Handled", algo);
+    dispatch({
+      type: "UPDATE_ALGORITHM",
+      algorithm: algo,
+    });
   };
+
+  const resetColor = () => {
+    dispatch({
+      type: "UPDATE_COLOR",
+      color: document.getElementById("color").value,
+    });
+  };
+
+  const handleRange = (_range) => {
+    let new_arr = [...myState.values];
+    for (let i = 0; i < new_arr.length; i++)
+      document.getElementById(i).style.transform = `translateX(${i * 11}px)`;
+
+    resetColor();
+
+    dispatch({
+      type: "UPDATE_RANGE",
+      range: _range,
+    });
+    dispatch({
+      type: "CHANGE_VALUES",
+    });
+  };
+
+  const handleColor = (_color) => {
+    dispatch({
+      type: "UPDATE_COLOR",
+      color: _color,
+    });
+  };
+
+  const handleSpeed = (_speed) => {
+    dispatch({
+      type: "UPDATE_SPEED",
+      speed: _speed,
+    });
+  };
+
+  useEffect(() => {
+    handleRange(30);
+  }, []);
+
+  useEffect(() => {
+    dispatch({
+      type: "UPDATE_COLOR",
+      color: document.getElementById("color").value,
+    });
+  }, [myState.values]);
+
+  const handleWidth = () => {
+    if (window.innerWidth > 1300) setMax(70);
+    else if (window.innerWidth > 1200) setMax(60);
+    else if (window.innerWidth > 1100) setMax(50);
+    else if (window.innerWidth > 900) setMax(45);
+    else if (window.innerWidth > 800) setMax(40);
+    else if (window.innerWidth > 500) setMax(30);
+    else setMax(20);
+  };
+
+  useEffect(() => {
+    handleWidth();
+    window.addEventListener("resize", handleWidth);
+    return () => window.removeEventListener("resize", handleWidth);
+  }, []);
 
   return (
     <div className="sidebar">
@@ -15,6 +88,7 @@ const Sidebar = () => {
           name="algo"
           id="algo"
           onChange={(e) => handleAlgo(e.target.value)}
+          disabled={myState.play ? true : false}
         >
           <option value="bubble">Bubble Sort </option>
           <option value="merge">Merge Sort</option>
@@ -33,9 +107,9 @@ const Sidebar = () => {
           id="slider"
           min={1}
           className="slider"
-          //   disabled={myState.play ? true : false}
-          //   max={max}
-          //   onChange={(e) => handleRange(e.target.value)}
+          disabled={myState.play ? true : false}
+          max={max}
+          onChange={(e) => handleRange(e.target.value)}
           valueLabelDisplay="auto"
         />
       </div>
@@ -45,8 +119,8 @@ const Sidebar = () => {
         <select
           name="color"
           id="color"
-          //   onChange={(e) => handleColor(e.target.value)}
-          //   disabled={myState.play ? true : false}
+          onChange={(e) => handleColor(e.target.value)}
+          disabled={myState.play ? true : false}
         >
           <option
             value="rgb(0, 149, 199)"
@@ -75,16 +149,18 @@ const Sidebar = () => {
           name="speed"
           defaultValue={100}
           id="speed"
-          //   onChange={(e) => handleSpeed(e.target.value)}
-          //   disabled={myState.play ? true : false}
+          onChange={(e) => handleSpeed(e.target.value)}
+          disabled={myState.play ? true : false}
         >
           <option value={500}>Slow</option>
           <option value={200}>Medium</option>
           <option value={100}>Fast</option>
+          <option value={20}>Super Fast</option>
+          <option value={5}>Ultra Fast</option>
         </select>
       </div>
     </div>
   );
-};
+}
 
 export default Sidebar;
