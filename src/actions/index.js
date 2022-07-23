83b92ed2224b671134f84f595dc7c0d2a3d85839
Divy <divyparekh1810@gmail.com>
2022-07-23 13:50:14 +0530

    Actions Defined

---
+++ src/actions/index.js
@@ -0,0 +1,42 @@
+export const updateRange = () => {
+  return {
+    type: "UPDATE_RANGE",
+  };
+};
+
+export const updateValues = () => {
+  return {
+    type: "UPDATE_VALUES",
+  };
+};
+
+export const updateSpeed = () => {
+  return {
+    type: "UPDATE_SPEED",
+  };
+};
+export const updateColor = () => {
+  return {
+    type: "UPDATE_COLOR",
+  };
+};
+export const updateAlgorithm = () => {
+  return {
+    type: "UPDATE_ALGORITHM",
+  };
+};
+export const updateTimeouts = () => {
+  return {
+    type: "UPDATE_TIMEOUTS",
+  };
+};
+export const changeValues = () => {
+  return {
+    type: "CHANGE_VALUES",
+  };
+};
+export const playPause = () => {
+  return {
+    type: "PLAY_PAUSE",
+  };
+};
